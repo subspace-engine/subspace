735c15a0a42637776a2e58aa59c98bad9453e88c
Mark Chimes <markjchimes@gmail.com>
2017-04-05 23:30:03 +0200

    Make a stupid terminal program that prints out the numbers from 1 to 10 based on moving the arrow keys.

---
+++ terminal.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/nsf/termbox-go"
+)
+
+func runTermbox() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	//termbox.Clear(termbox.ColorGreen, termbox.ColorBlue)
+	termbox.Flush()
+
+	numX := 0
+	numY := 0
+	loop:
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				break loop
+			case termbox.KeyF1:
+				break loop
+			case termbox.KeyArrowUp:
+				numY -= 1
+			case termbox.KeyArrowDown:
+				numY += 1
+			case termbox.KeyArrowLeft:
+				numX -= 1
+			case termbox.KeyArrowRight:
+				numX += 1
+			}
+		}
+		// termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		termbox.SetCell(numX, numY, rune(48+((numX + numY)%10)), termbox.ColorGreen, termbox.ColorDefault)
+		termbox.Flush()
+	}
+
+	defer termbox.Close()
+}
+
+func main() {
+	runTermbox()
+
+}
